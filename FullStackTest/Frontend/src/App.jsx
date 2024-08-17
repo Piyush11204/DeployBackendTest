4876c5381b9b7923d913b3ca037c8bb21d31550a
Piyush11204 <138059020+Piyush11204@users.noreply.github.com>
2024-08-17 12:55:43 +0530

    fullstack commit

---
+++ FullStackTest/Frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from "axios"
+function App() {
+  const [jokes,setjokes ] = useState([])
+
+  useEffect(()=>{
+    axios.get("/api/jokes")
+    .then((Response)=>{
+      setjokes(Response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  })
+
+  return (
+    <>
+     <h1 className="jokes">
+      Your Life is a Joke
+     </h1>
+     <p>Jokes:{jokes.length}</p>
+     {
+      jokes.map((jokes,index)=>(
+        <div key ={jokes.id}>
+        <h2>{jokes.joke}</h2>
+      </div>
+        
+      ))
+    
+    }
+    </>
+  )
+}
+
+export default App
